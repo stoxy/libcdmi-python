8798f674b762d6f751d4737397899b0cf22c2809
Andrei Sosnin <andrei.sosnin@gmail.com>
2013-07-24 09:24:36 +0300

    Refactoring: eliminate unneeded abstraction (STOX-9)

--- libcdmi/connection.py
+++ libcdmi/connection.py
@@ -9,20 +9,6 @@ except ImportError:
 from libcdmi.common import CDMI_OBJECT, CDMI_CONTAINER, HEADER_CDMI_VERSION
 
 
-class ObjectProxy(object):
-    pass
-
-
-class Container(ObjectProxy):
-    headers = {}
-    mime_type = CDMI_CONTAINER
-
-
-class Blob(ObjectProxy):
-    headers = {}
-    mime_type = CDMI_OBJECT
-
-
 class Connection(object):
 
     credentials = None
@@ -43,8 +29,7 @@ class Connection(object):
         """Get meta-information about a blob. Returns JSON-encoded metadata."""
         headers = self._make_headers({'Accept': CDMI_OBJECT})
         response = requests.head(self.endpoint + resource,
-                                 auth=self.credentials,
-                                 headers=headers)
+                                 auth=self.credentials, headers=headers)
         return response.headers
 
     def get(self, resource):
@@ -52,51 +37,42 @@ class Connection(object):
         # put relevant headers
         headers = self._make_headers({'Accept': CDMI_OBJECT})
         response = requests.get(self.endpoint + resource,
-                                auth=self.credentials,
-                                headers=headers)
+                                auth=self.credentials, headers=headers)
         return response.json()
 
     def create_container(self, resource, metadata={}):
         """Create a new container"""
-        op = Container()
-        op.headers = self._make_headers({'Accept': op.mime_type,
-                                         'Content-Type': op.mime_type})
+        headers = self._make_headers({'Accept': CDMI_CONTAINER,
+                                      'Content-Type': CDMI_CONTAINER})
+        data = {'metadata': metadata}
 
-        op.data = {'metadata': metadata}
-
-        res = requests.put(self.endpoint + resource,
-                           json.dumps(op.data),
-                           auth=self.credentials,
-                           headers=op.headers)
+        res = requests.put(self.endpoint + resource, json.dumps(data),
+                           auth=self.credentials, headers=headers)
         return res.json()
 
     update_container = create_container
 
     def create_blob(self, resource, local_filename, mimetype='text/plain',
                     metadata={}):
-        op = Blob()
-        op.headers = self._make_headers({'Accept': op.mime_type,
-                                         'Content-Type': op.mime_type})
-
-        op.data = {'mimetype': mimetype,
-                   'metadata': metadata}
+        headers = self._make_headers({'Accept': CDMI_OBJECT,
+                                      'Content-Type': CDMI_OBJECT})
+        data = {'mimetype': mimetype,
+                'metadata': metadata}
 
         with open(local_filename, 'rb') as input_file:
             try:
                 content = input_file.read()
                 unicode(content, 'utf-8')
-                op.data['valuetransferencoding'] = 'utf-8'
+                data['valuetransferencoding'] = 'utf-8'
             except UnicodeDecodeError:
                 input_file.seek(0)
                 content = base64.b64encode(input_file.read())
-                op.data['valuetransferencoding'] = 'base64'
+                data['valuetransferencoding'] = 'base64'
 
-        op.data['value'] = content
+        data['value'] = content
 
-        res = requests.put(self.endpoint + resource,
-                           json.dumps(op.data),
-                           auth=self.credentials,
-                           headers=op.headers)
+        res = requests.put(self.endpoint + resource, json.dumps(data),
+                           auth=self.credentials, headers=headers)
         return res.json()
 
     update_blob = create_blob
