a8fd1f8a7e690e68abaaaee60d45ab556d3f015c
Andrei Sosnin <andrei.sosnin@gmail.com>
2013-07-25 18:48:17 +0300

    Remove arguably unneeded namespace declaration (STOX-10)

--- setup.py
+++ setup.py
@@ -8,7 +8,6 @@ setup(
     author="Ilja Livenson and Co",
     author_email="ilja.livenson@gmail.com",
     packages=find_packages(),
-    namespace_packages=['stoxy'],
     zip_safe=False,  # martian grok scan is incompatible with zipped eggs
     install_requires=[
         "setuptools",  # Redundant but removes a warning
